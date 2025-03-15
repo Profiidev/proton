9d96f9ff6e4611e6ab5c3302d922e7b3d28e08f4
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-03-15 21:47:44 +0100

    feat: added auth window

--- app/src-tauri/src/lib.rs
+++ app/src-tauri/src/lib.rs
@@ -1,14 +1,32 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use tauri::{AppHandle, Url, WebviewWindowBuilder};
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn auth(handle: AppHandle) {
+  WebviewWindowBuilder::new(
+    &handle,
+    "auth",
+    tauri::WebviewUrl::External(
+      Url::parse_with_params(
+        "https://login.microsoftonline.com/consumers/oauth2/v2.0/authorize",
+        vec![
+          ("client_id", "dd35660a-6381-41f8-bb34-2a36669581d0"),
+          ("response_type", "code"),
+          ("scope", "XboxLive.signin offline_access"),
+        ],
+      )
+      .unwrap(),
+    ),
+  )
+  .inner_size(600.0, 600.0)
+  .build()
+  .unwrap();
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  tauri::Builder::default()
+    .plugin(tauri_plugin_opener::init())
+    .invoke_handler(tauri::generate_handler![auth])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
