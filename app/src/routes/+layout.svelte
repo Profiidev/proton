db1e5c6abe3293d533b8ff0482a92a5f568ed600
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-03-18 15:18:15 +0100

    feat: added titlebar

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,6 +1,19 @@
 <script lang="ts">
+  import { setMode, ModeWatcher } from "mode-watcher";
   import "../app.css";
+  import Header from "$lib/components/Header.svelte";
+    import ProfileSelector from "$lib/components/ProfileSelector.svelte";
   let { children } = $props();
+
+  setMode("dark");
 </script>
 
-{@render children()}
+<ModeWatcher />
+
+<div class="h-full flex flex-col">
+  <Header />
+  <ProfileSelector />
+  <main class="flex-grow-1">
+    {@render children()}
+  </main>
+</div>
