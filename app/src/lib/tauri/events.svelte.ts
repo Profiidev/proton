9ed7d9d2777b1cd69cf90eb15ab42f8935cd2e32
GitHub Action <action@github.com>
2025-03-20 20:13:51 +0000

    Fix code style issues with Prettier

--- app/src/lib/tauri/events.svelte.ts
+++ app/src/lib/tauri/events.svelte.ts
@@ -10,7 +10,7 @@ export const ACCOUNT_LOGIN_STATUS_EVENT = 'account-login-status';
 export enum VersionCheckStatus {
   Manifest = 'Manifest',
   Assets = 'Assets',
-  Java = 'Java',
+  Java = 'Java'
 }
 
 export const VERSION_CHECK_STATUS_EVENT = 'version-check-status';
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-    import { account_active } from '$lib/tauri/account.svelte';
+  import { account_active } from '$lib/tauri/account.svelte';
   import { invoke } from '@tauri-apps/api/core';
   import { Button } from 'positron-components/components/ui';
-  
+
   let active = $derived(account_active.value);
 
   const download = async () => {
     let res = await invoke('versions_launch', {
       version: '1.0',
-      account: active,
+      account: active
     });
     console.log(res);
   };
