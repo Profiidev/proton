3c65e1c962f8303c1267171faa668b089c132b0a
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-03-21 17:47:34 +0100

    fix: no accounts not shown correctly in popover

--- app/src/lib/components/Sidebar.svelte
+++ app/src/lib/components/Sidebar.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { Button } from 'positron-components/components/ui';
   import AccountSelector from './account/AccountSelector.svelte';
-  import { Home } from 'lucide-svelte';
+  import { Home, Settings } from 'lucide-svelte';
   import { goto } from '$app/navigation';
 </script>
 
@@ -13,6 +13,14 @@
   >
     <Home class="size-8!" />
     <p>Home</p>
+  </Button>
+  <Button
+    variant="ghost"
+    class="h-12 w-full justify-start p-2"
+    onclick={() => goto('/settings/accounts')}
+  >
+    <Settings class="size-8!" />
+    <p>Settings</p>
   </Button>
   <AccountSelector />
 </div>
--- app/src/lib/components/account/AccountSelector.svelte
+++ app/src/lib/components/account/AccountSelector.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import { goto } from '$app/navigation';
   import {
     account_active,
     account_list,
@@ -15,11 +14,6 @@
   let accounts: Accounts | undefined = $derived(account_list.value);
   let active: string | undefined = $derived(account_active.value);
   let open = $state(false);
-
-  const edit = (e: Event) => {
-    e.stopPropagation();
-    goto('/settings/accounts');
-  };
 </script>
 
 <Popover.Root bind:open>
@@ -29,8 +23,7 @@
     {#if active && accounts && accounts[active]}
       {@render account({
         info: accounts[active],
-        onclick: () => {},
-        edit: true
+        onclick: () => {}
       })}
     {:else}
       <Button class="h-12 w-full justify-start p-2" variant="ghost">
@@ -38,12 +31,11 @@
           <CircleHelp />
         </div>
         <p class="truncate text-start text-sm">No account active</p>
-        {@render edit_btn()}
       </Button>
     {/if}
   </Popover.Trigger>
   <Popover.Content class="flex w-50 flex-col p-1">
-    {#if accounts}
+    {#if accounts && Object.entries(accounts).length > 0}
       {#each Object.entries(accounts).sort( (a, b) => a[0].localeCompare(b[0]) ) as [id, info]}
         {#if info}
           {@render account({
@@ -68,19 +60,17 @@
         {/if}
       {/each}
     {:else}
-      <p>No Accounts</p>
+      <p class="p-2">No Accounts</p>
     {/if}
   </Popover.Content>
 </Popover.Root>
 
 {#snippet account({
   info,
-  onclick,
-  edit
+  onclick
 }: {
   info: ProfileInfo;
   onclick: () => void;
-  edit?: boolean;
 })}
   {@const skin_url = info.skins.find((s) => s.state === State.Active)?.url}
   <Button class="h-12 w-full justify-start p-2" variant="ghost" {onclick}>
@@ -89,19 +79,5 @@
       <p class="truncate text-start text-sm">{info.name}</p>
       <p class="text-muted-foreground truncate text-start text-sm">{info.id}</p>
     </div>
-    {#if edit}
-      {@render edit_btn()}
-    {/if}
-  </Button>
-{/snippet}
-
-{#snippet edit_btn()}
-  <Button
-    size="icon"
-    variant="ghost"
-    class="hover:bg-muted-foreground size-8 min-w-8"
-    onclick={edit}
-  >
-    <Settings class="size-5!" />
   </Button>
 {/snippet}
