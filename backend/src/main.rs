42f32e101b0fa61d7df663635a5c7dcf6027134e
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-04-11 16:55:48 +0200

    refactor: removed loki

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -14,23 +14,13 @@ async fn rocket() -> _ {
   #[cfg(debug_assertions)]
   dotenv().ok();
 
-  #[allow(unused)]
-  let url = std::env::var("LOKI_URL").expect("Failed to load LOKI_URL");
   let level = std::env::var("RUST_LOG")
     .unwrap_or("warn".into())
     .parse()
     .expect("Failed to parse RUST_LOG");
-  let application = std::env::var("LOKI_APP").expect("Failed to load LOKI_APP");
-  let environment = std::env::var("LOKI_ENV").expect("Failed to load LOKI_ENV");
-  #[allow(unused)]
-  let log_labels: HashMap<String, String> = HashMap::from_iter([
-    ("application".into(), application),
-    ("environment".into(), environment),
-  ]);
 
   Dispatch::new()
     .chain(Box::new(env_logger::builder().build()) as Box<dyn log::Log>)
-    //.chain(Box::new(log_loki::LokiBuilder::new(url, log_labels).build()) as Box<dyn log::Log>)
     .level(level)
     .apply()
     .expect("Failed to initialize logger");
