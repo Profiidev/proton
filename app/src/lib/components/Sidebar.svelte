eee5fa2ae2be09cc89a98618b087a8278f30681e
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-07-24 13:28:17 +0200

    style: added animation to nav bar

--- app/src/lib/components/Sidebar.svelte
+++ app/src/lib/components/Sidebar.svelte
@@ -2,8 +2,10 @@
   import { Button } from 'positron-components/components/ui';
   import AccountSelector from './account/AccountSelector.svelte';
   import { Home, LibraryBig, Settings } from '@lucide/svelte';
-  import { goto } from '$app/navigation';
   import { page } from '$app/state';
+  import { cn } from 'positron-components/utils';
+  import { crossfade } from 'svelte/transition';
+  import { cubicInOut } from 'svelte/easing';
 
   interface Props {
     collapsed: boolean;
@@ -28,22 +30,37 @@
       icon: Settings
     }
   ];
+
+  const [send, receive] = crossfade({
+    duration: 250,
+    easing: cubicInOut
+  });
 </script>
 
 <div class="flex h-full flex-col p-2">
   {#each btns as btn}
+    {@const isActive = page.url.pathname.startsWith(btn.url)}
     <Button
+      href={btn.url}
       variant="ghost"
-      class={'h-12 w-full justify-start p-2!' +
-        (page.url.pathname.startsWith(btn.url)
-          ? ' bg-accent text-accent-foreground'
-          : '')}
-      onclick={() => goto(btn.url)}
+      class={cn(
+        'relative h-12 w-full justify-start p-2!',
+        !isActive && 'hover:underline'
+      )}
     >
-      <btn.icon class="size-full max-w-8" />
-      {#if !collapsed}
-        <p class="truncate">{btn.title}</p>
+      {#if isActive}
+        <div
+          class="bg-muted absolute inset-0 rounded-md"
+          in:send={{ key: 'active-sidebar-tab' }}
+          out:receive={{ key: 'active-sidebar-tab' }}
+        ></div>
       {/if}
+      <div class="relative flex w-full items-center gap-2">
+        <btn.icon class="size-full max-w-8" />
+        {#if !collapsed}
+          <p class="truncate">{btn.title}</p>
+        {/if}
+      </div>
     </Button>
   {/each}
   <AccountSelector {collapsed} />
