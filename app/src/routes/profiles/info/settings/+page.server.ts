d663b741d387d65ebc34a8cb1fcca6673aa49e45
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-07-30 22:22:26 +0200

    ci: fix tsc dir

--- app/src/routes/profiles/info/settings/+page.server.ts
+++ app/src/routes/profiles/info/settings/+page.server.ts
@@ -1,5 +1,5 @@
 import { superValidate } from 'sveltekit-superforms';
-import type { PageServerLoad } from '../$types';
+import type { PageServerLoad } from './$types';
 import { zod } from 'sveltekit-superforms/adapters';
 import { profileEditSchema } from './schema.svelte';
 
