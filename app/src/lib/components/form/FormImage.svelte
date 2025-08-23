48d96fe58e223e59c2719e072181c355c1139a08
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-08-23 11:45:37 +0200

    refactor: auto save for general profile settings

--- app/src/lib/components/form/FormImage.svelte
+++ app/src/lib/components/form/FormImage.svelte
@@ -1,14 +1,13 @@
 <script lang="ts" generics="S extends FormRecord = FormRecord">
-  import { Form, Avatar, Input } from 'positron-components/components/ui';
+  import { Form } from 'positron-components/components/ui';
   import type { HTMLInputAttributes } from 'svelte/elements';
-  import { Upload } from '@lucide/svelte';
   import {
     type FormPath,
     type FormRecord,
     type SuperForm
   } from 'positron-components/components/form';
   import { get } from 'svelte/store';
-  import { cn } from 'positron-components/utils';
+  import ImageInput from './ImageInput.svelte';
 
   interface Props {
     formData: SuperForm<S>;
@@ -22,68 +21,23 @@
   let {
     formData: form,
     key,
-    disabled,
-    class: className,
-    label,
-    previewSrc,
     ...restProps
   }: HTMLInputAttributes & Props = $props();
 
   const { form: formData } = $derived(form);
 
-  let files = $state<FileList | undefined>();
-  let src = $state('');
-
-  $effect(() => {
-    if (files && files.length > 0) {
-      src = URL.createObjectURL(files[0]);
-    } else if (previewSrc) {
-      src = previewSrc;
-    }
-  });
-
-  $effect(() => {
+  const fileChange = (file?: File) => {
     let store = get(formData);
-    if (files && files.length > 0) {
-      // @ts-ignore
-      store[key] = files[0];
-    } else {
-      // @ts-ignore
-      store[key] = undefined;
-    }
+    // @ts-ignore
+    store[key] = file;
     formData.set(store);
-  });
+  };
 </script>
 
 <Form.Field {form} name={key} class="gap-1/2 grid">
   <Form.Control>
-    {#snippet children({ props })}
-      {#if label}
-        <Form.Label>{label}</Form.Label>
-      {/if}
-      <label for={key} class="hover:cursor-pointer">
-        <Avatar.Root
-          class={cn(
-            'ring-accent ring-offset-background size-20 rounded-md ring-2 ring-offset-2',
-            className
-          )}
-        >
-          <Avatar.Image class="object-cover" {src} />
-          <Avatar.Fallback class="rounded-md">
-            <Upload class="size-4" />
-            <span class="sr-only">Upload image</span>
-          </Avatar.Fallback>
-        </Avatar.Root>
-      </label>
-      <Input
-        {...restProps}
-        id={key}
-        type="file"
-        bind:files
-        {disabled}
-        class="hidden"
-        accept="image/png, image/jpeg, image/jpg"
-      />
+    {#snippet children()}
+      <ImageInput {key} {...restProps} {fileChange} labelComp={Form.Label} />
     {/snippet}
   </Form.Control>
   <Form.FieldErrors />
--- app/src/routes/profiles/info/settings/general/+page.svelte
+++ app/src/routes/profiles/info/settings/general/+page.svelte
@@ -4,42 +4,26 @@
     profile_get_icon,
     profile_update,
     profile_update_icon,
-    ProfileError
+    ProfileError,
+    type Profile
   } from '$lib/tauri/profile.svelte';
-  import type { PageServerData } from './$types';
-  import {
-    BaseForm,
-    FormInput,
-    type FormType
-  } from 'positron-components/components/form';
-  import { profileEditSchema } from './schema.svelte';
-  import { type SvelteComponent } from 'svelte';
-  import FormImage from '$lib/components/form/FormImage.svelte';
+  import { Multiselect } from 'positron-components/components/table';
   import { getProfile } from '../../store.svelte';
-  import { file_to_bytes } from '$lib/util.svelte';
   import { toast } from 'svelte-sonner';
   import {
     loader_version_list,
     version_list
   } from '$lib/tauri/versions.svelte';
-  import FormSelectUpdate from '$lib/components/form/FormSelectUpdate.svelte';
-
-  interface Props {
-    data: PageServerData;
-  }
-
-  let { data }: Props = $props();
+  import { Input, Label } from 'positron-components/components/ui';
+  import ImageInput from '$lib/components/form/ImageInput.svelte';
 
   let profile = $derived(getProfile());
-  let isLoading = $state(false);
-  let form = $state<SvelteComponent>();
-  let setValue: (value: any) => void = $derived(
-    form?.setValue || (() => undefined)
-  );
+
   let versions = $state<{ label: string; value: string }[]>([]);
-  let selectedVersion = $state<[string]>();
+  let selectedVersion = $state<[string]>(['']);
   let loader_versions = $state<{ label: string; value: string }[]>([]);
-  let selectedLoaderVersion = $state<[string]>();
+  let selectedLoaderVersion = $state<[string]>(['']);
+
   $effect(() => {
     if (!profile) {
       return;
@@ -83,23 +67,14 @@
       });
   });
 
-  const profileEdit = {
-    form: data.profileEdit,
-    schema: profileEditSchema
-  };
-
   let formFilePreview = $state('');
   $effect(() => {
     if (profile) {
       (async () => {
-        let profileData = {
-          ...profile,
-          version: [profile.version],
-          loader_version: [profile.loader_version]
-        };
-        setTimeout(() => {
-          setValue(profileData);
-        });
+        selectedVersion = [profile.version];
+        if (profile.loader !== LoaderType.Vanilla && profile.loader_version) {
+          selectedLoaderVersion = [profile.loader_version];
+        }
 
         let icon = await profile_get_icon(profile.id);
         if (icon) {
@@ -109,91 +84,95 @@
     }
   });
 
-  const updateProfile = async (form: FormType<any>) => {
-    if (!profile) {
-      return { error: 'Profile not found' };
+  $effect(() => {
+    selectedVersion;
+    selectedLoaderVersion;
+
+    if (!profile) return;
+    let update = false;
+    let updated_profile = { ...profile };
+
+    if (profile.version !== selectedVersion[0] && selectedVersion[0]) {
+      update = true;
+      updated_profile.version = selectedVersion[0];
     }
 
     if (
-      profile.loader !== LoaderType.Vanilla &&
-      (!form.data.loader_version || form.data.loader_version.length !== 1)
+      profile?.loader !== LoaderType.Vanilla &&
+      profile?.loader_version !== selectedLoaderVersion[0] &&
+      selectedLoaderVersion[0]
     ) {
-      return { field: 'loader_version', error: 'Loader version is required' };
+      update = true;
+      updated_profile.loader_version = selectedLoaderVersion[0];
     }
 
-    form.data.id = profile.id;
-    form.data.version = form.data.version[0];
-    form.data.loader_version = form.data.loader_version
-      ? form.data.loader_version[0]
-      : undefined;
+    if (update) {
+      send_update(updated_profile);
+    }
+  });
 
-    if (form.data.icon) {
-      let bytes = await file_to_bytes(form.data.icon);
-      let res = await profile_update_icon(profile.id, bytes);
-
+  const send_update = (profile: Profile) => {
+    profile_update(profile).then((res) => {
       if (res === ProfileError.Other) {
-        return { error: 'Failed to update profile icon' };
-      } else if (res === ProfileError.InvalidImage) {
-        return { field: 'icon', error: 'Invalid image' };
+        toast.error('Failed to update profile');
       }
-    }
+    });
+  };
 
-    let res = await profile_update(form.data);
+  const update_icon = async (file?: File) => {
+    if (!profile || !file) return;
+    let bytes = new Uint8Array(await file.arrayBuffer());
+
+    let res = await profile_update_icon(profile.id, bytes);
     if (res === ProfileError.Other) {
-      return { error: 'Failed to update profile' };
-    } else {
-      toast.success('Profile updated successfully');
+      toast.error('Failed to update profile icon');
     }
-
-    return undefined;
   };
 </script>
 
 {#if profile}
-  <div>
-    <BaseForm
-      form={profileEdit}
-      confirm="Save"
-      bind:isLoading
-      bind:this={form}
-      onsubmit={updateProfile}
-    >
-      {#snippet children({ props })}
-        <FormImage
-          key="icon"
-          class="size-20"
-          type="file"
-          previewSrc={formFilePreview}
-          {...props}
-        />
-        <FormInput
-          label="Name"
-          placeholder="Profile Name"
-          key="name"
-          {...props}
-        />
-        <FormSelectUpdate
-          bind:val={selectedVersion}
-          label="Version"
-          key="version"
+  <div class="flex gap-4">
+    <div class="flex w-64 flex-col gap-2">
+      <Label>Profile Name</Label>
+      <Input
+        placeholder="Profile Name"
+        value={profile.name}
+        onfocusout={(e) => {
+          let name = (e.target as HTMLInputElement).value;
+          if (!name) {
+            toast.error('Profile name cannot be empty');
+            return;
+          }
+          if (name !== profile.name) {
+            send_update({ ...profile, name });
+          }
+        }}
+      />
+      <Label>Version</Label>
+      <Multiselect
+        label="Version"
+        single={true}
+        data={versions}
+        bind:selected={selectedVersion}
+      />
+      {#if profile.loader !== LoaderType.Vanilla}
+        <Label>Loader Version</Label>
+        <Multiselect
           single={true}
-          data={versions}
-          {...props}
+          data={loader_versions}
+          label="Loader Version"
+          bind:selected={selectedLoaderVersion}
         />
-        {#if profile.loader !== LoaderType.Vanilla}
-          <FormSelectUpdate
-            bind:val={selectedLoaderVersion}
-            label="Loader Version"
-            key="loader_version"
-            single={true}
-            data={loader_versions}
-            {...props}
-          />
-        {/if}
-      {/snippet}
-      {#snippet footer({ children })}
-        {@render children()}
-      {/snippet}
-    </BaseForm>
+      {/if}
+    </div>
+    <div class="flex flex-col">
+      <ImageInput
+        class="mt-3"
+        key="profile-icon"
+        label="Profile Icon"
+        fileChange={update_icon}
+        previewSrc={formFilePreview}
+      />
+    </div>
   </div>
 {/if}
