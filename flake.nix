ecac5e2f111f07837776a7747ab2e1e68ec20e3f
ProfiDev <92174452+Profiidev@users.noreply.github.com>
2025-08-26 23:05:37 +0200

    Updated version to v0.2.2

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
       {
         packages.default = pkgs.rustPlatform.buildRustPackage rec {
           pname = "proton";
-          version = "0.2.1";
+          version = "0.2.2";
 
           src = ./.;
 
